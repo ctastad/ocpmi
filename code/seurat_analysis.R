e7dbf84e710b149f2e7bd8d3975b1369f12487ef
tasta005 <tasta005@umn.edu>
2019-06-11 19:40:15 -0500

    add .R notation for rmarkdown rendering
    Former-commit-id: ec1de0edb27679341f873a5b029877e237321473

--- code/seurat_analysis.R
+++ code/seurat_analysis.R
@@ -1,4 +1,16 @@
-# Dependencies, Initialize
+#' ---
+#' title: "Default Seurat Analysis"
+#' author: "Christopher Tastad"
+#' date: \today
+#' bibliography: library.bib
+#' output:
+#'     html_document:
+#'         toc: yes
+#'         toc_float: yes
+#'         theme: paper
+#' ---
+
+#' # Dependencies, Initialize
 
 library(dplyr)
 library(Seurat)
@@ -9,17 +21,17 @@ pt59 <- CreateSeuratObject(counts = pt59.data, project = "pt59", min.cells = 3, 
 pt59
 
 
-# Survey data
+#' # Survey data
 
 pt59.data[c("CD3D","TCL1A","MS4A1"), 1:30]
 
 
-# Pre-processing workflow
+#' # Pre-processing workflow
 
 pt59[["percent.mt"]] <- PercentageFeatureSet(object = pt59, pattern = "^MT-")
 
 
-# Visualize QC metrics
+#' # Visualize QC metrics
 
 head(x = pt59@meta.data, 5)
 VlnPlot(object = pt59, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
@@ -29,11 +41,11 @@ CombinePlots(plots = list(plot1,plot2))
 
 pt59 <- subset(x = pt59, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)
 
-# Normalizing the data
+#' # Normalizing the data
 
 pt59 <- NormalizeData(object = pt59, normalization.method = "LogNormalize", scale.factor = 1e4)
 
-# Identification of highly variable features
+#' # Identification of highly variable features
 
 pt59 <- FindVariableFeatures(object = pt59, selection.method = 'vst', nfeatures = 2000)
 top10 <- head(x = VariableFeatures(object = pt59), 10)
@@ -41,12 +53,12 @@ plot1 <- VariableFeaturePlot(object = pt59)
 plot2 <- LabelPoints(plot = plot1, points = top10, repel = T)
 CombinePlots(plots = list(plot1, plot2))
 
-# Scaling the data
+#' # Scaling the data
 
 all.genes <- rownames(x = pt59)
 pt59 <- ScaleData(object = pt59, features = all.genes)
 
-# Linear dimensional reduction
+#' # Linear dimensional reduction
 
 pt59 <- RunPCA(object = pt59, features = VariableFeatures(object = pt59))
 print(x = pt59[['pca']], dims = 1:5, nfeatures = 5)
@@ -55,20 +67,20 @@ DimPlot(object = pt59, reduction = 'pca')
 DimHeatmap(object = pt59, dims = 1, cells = 500, balanced = TRUE)
 DimHeatmap(object = pt59, dims = 1:15, cells = 500, balanced = TRUE)
 
-# Determine dimensionality
+#' # Determine dimensionality
 
 pt59 <- JackStraw(object = pt59, num.replicate = 100)
 pt59 <- ScoreJackStraw(object = pt59, dims = 1:20)
 JackStrawPlot(object = pt59, dims = 1:20)
 ElbowPlot(object = pt59)
 
-# Clustering the cells
+#' # Clustering the cells
 
 pt59 <- FindNeighbors(object = pt59, dims = 1:20)
 pt59 <- FindClusters(object = pt59, resolution = 0.5)
 head(x = Idents(object = pt59), 5)
 
-# Non-linear dimensional reduction
+#' # Non-linear dimensional reduction
 
 pt59 <- RunUMAP(object = pt59, dims = 1:20)
 DimPlot(object = pt59, reduction = 'umap')
