a693e96c6fd24bdd72761dea14f114a56eaf54d6
tasta005 <tasta005@umn.edu>
2019-08-18 21:59:44 -0500

    minor update to full_send_vars.sh; deleted some pilot output to be redone
    Former-commit-id: f174ea55216d00a9ea5589fbf4c4f07e78b14a8c
    Former-commit-id: 2daab1a8432796edf897b72ce1b56579e2b2361b [formerly 6c9c8d9511bb5aee5b348f7e98de1140cf274bde]
    Former-commit-id: 9f7ea52a7b5be75f4f9a067b07e904df9df1fe68
    Former-commit-id: 8d263ab25ccc90ab11dc2c2a8ab6b78d2ee65b10

--- results/excavator2/ocpmi_1kgp_compare_test/.tmp/ParallelAnalyzePerla.sh
+++
@@ -1,1 +0,0 @@
-perl /panfs/roc/groups/11/starrt2/tasta005/ocpmi/methods/EXCAVATOR2_Package_v1.1.2/lib/perl/AnalyzePerla.pl /home/starrt2/tasta005/ocpmi/results/excavator2/ocpmi_1kgp_compare_test/.tmp/ExperimentalFileAnalysis.txt_1.txt --assembly hg38 --output /home/starrt2/tasta005/ocpmi/results/excavator2/ocpmi_1kgp_compare_test --target hg38_target --mode pooling
