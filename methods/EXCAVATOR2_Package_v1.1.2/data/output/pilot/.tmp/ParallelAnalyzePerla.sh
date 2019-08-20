083752905561cc692f14d0602dbdac87ef4e58d1
tasta005 <tasta005@umn.edu>
2019-08-19 19:24:17 -0500

    continued tinkering with full_send, updated file movement syntax issues
    Former-commit-id: 133835a6935688dc939028a17e387b84baca71c5
    Former-commit-id: e187ac302409e8c0e2b78c189c21d93972029673

--- methods/EXCAVATOR2_Package_v1.1.2/data/output/pilot/.tmp/ParallelAnalyzePerla.sh
+++
@@ -1,1 +0,0 @@
-perl /panfs/roc/groups/11/starrt2/tasta005/ocpmi/methods/EXCAVATOR2_Package_v1.1.2/lib/perl/AnalyzePerla.pl data/output/pilot/.tmp/ExperimentalFileAnalysis.50000.txt_1.txt --assembly hg38 --output data/output/pilot --target pilot --mode pooling
--- methods/EXCAVATOR2_Package_v1.1.2/full_send.sh
+++ methods/EXCAVATOR2_Package_v1.1.2/full_send.sh
@@ -14,15 +14,15 @@ runDa3=1 # step 3 EXCAVATORDataAnalysis.pl
 # Loop to iterate through each bam to create string for FilePrepare and FileAnalysis configs
 count=1
 for i in *.{bam,cram}; do
-    echo "$PWD/$i" "data/prepare/$expName/${i%.*}" "${i%.*}" >> ExperimentalFilePrepare.txt
-    echo "T$count" "data/prepare/$expName/${i%.*}" "${i%.*}" >> ExperimentalFileAnalysis.txt
+    echo "$PWD/$i" "data/prepare/$expName/${i%.*}" "${i%.*}" >> ExperimentalFilePrepare.$expName.txt
+    echo "T$count" "data/prepare/$expName/${i%.*}" "${i%.*}" >> ExperimentalFileAnalysis.$expName.txt
     mkdir -p $HOME$excv2Dir/data/prepare/$expName/${i%.*}
     count=$[count+1]
 done
 
 # Function to remove leading path from sshfs local mount path and create control
-sed -i -e 's/\/home\/chris/~/g' ./ExperimentalFilePrepare.txt
-sed -i '$s/T[[:digit:]]*/C1/g' ./ExperimentalFileAnalysis.txt
+sed -i -e 's/\/home\/chris/~/g' ./ExperimentalFilePrepare.$expName.txt
+sed -i '$s/T[[:digit:]]*/C1/g' ./ExperimentalFileAnalysis.$expName.txt
 
 # Relocate configs to Excavator working dir
 mv ExperimentalFile* $HOME$excv2Dir
@@ -32,25 +32,37 @@ mkdir -p $HOME$excv2Dir/config_files/$expName
 # Generate SourceTarget.txt and execute step 1 TargetPerla.pl
 cd $HOME$excv2Dir
 if [[ $runTp1 = 1 ]]; then
-    echo "$HOME$bigWig" "$HOME$genRef" > SourceTarget.txt
-    perl TargetPerla.pl SourceTarget.txt $HOME$bedFile $target $window $refAssm;
+    echo "$HOME$bigWig" "$HOME$genRef" > SourceTarget.$expName.txt
+    perl TargetPerla.pl SourceTarget.$expName.txt $HOME$bedFile $target $window $refAssm;
 else
     echo "Skipping step 1 TargetPerla.pl"
 fi
 
 # Executing step 2 EXCAVATORDataPrepare.pl
 if [[ $runDp2 = 1 ]]; then
-    perl EXCAVATORDataPrepare.pl -v ExperimentalFilePrepare.txt --processors $numProc --target $target --assembly $refAssm
+    perl EXCAVATORDataPrepare.pl -v ExperimentalFilePrepare.$expName.txt --processors $numProc --target $target --assembly $refAssm
 else
     echo "Skipping step 2 EXCAVATORDataPrepare.pl"
 fi
 
 # Executing step 3 EXCAVATORDataAnalysis.pl
 if [[ $runDa3 = 1 ]]; then
-    perl EXCAVATORDataAnalysis.pl -v ExperimentalFileAnalysis.txt --processors $numProc --target $target --assembly $refAssm --output $HOME$resDir/$expName --mode $anMode
+    perl EXCAVATORDataAnalysis.pl -v ExperimentalFileAnalysis.$expName.txt --processors $numProc --target $target --assembly $refAssm --output $HOME$resDir/$expName --mode $anMode
 else
     echo "Skipping step 3 EXCAVATORDataAnalysis.pl"
 fi
 
 # Copy config files for record
-cp {ExperimentalFile*, SourceTarget.txt, full_send_vars.sh, ParameterFile.txt} $HOME$excv2Dir/config_files/$expName
+cp ParameterFile.txt ParameterFile.$expName.txt
+cp full_send_vars.sh full_send_vars.$expName.sh
+mv *.{$expname}.* $HOME$excv2Dir/config_files/$expName
+
+# Cleanup bad file naming convention
+cd $HOME$resDir/$expName/Plots
+for i in */ ; do
+    cd $i
+    for j in {1..9}; do
+        mv PlotResults_chr$j.pdf PlotResults_chr0$j.pdf
+    done
+    cd ../
+done
