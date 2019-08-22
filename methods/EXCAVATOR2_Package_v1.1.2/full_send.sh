35c9f1799d82d8e4c39ca30ebfbb818c13115573
tasta005 <tasta005@umn.edu>
2019-08-21 22:21:03 -0500

    no idea; eod
    Former-commit-id: bf092696d01178f1960fb15a0407aac209b11994
    Former-commit-id: 96a19532d2f0aa7c3dc33ac128694fe6fab6a1d8 [formerly 2122ccbb48cca5363a73299c236e2eee76df3481]
    Former-commit-id: 51a469fa4f8406c5077caa23d0e3c423a977ac86
    Former-commit-id: e8892768950789277b9ac3384acdd8f2447ba911

--- methods/EXCAVATOR2_Package_v1.1.2/full_send.sh
+++ methods/EXCAVATOR2_Package_v1.1.2/full_send.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-# This script is meant to prep the configuration and execution of the Excavator2 pipeline. It auto generates two text files that serve as configs for the pipeline scripts. Proper use of this script is paired with the full_send_vars.sh variable file to indicate pointers and variable names. Both should be placed in the Excavator2 working directory with this script being executed from there.
+# This script is meant to prep the configuration and execution of the Excavator2 pipeline. It auto generates two text files that serve as configs for the pipeline scripts. Proper use of this script is paired with the full_send_vars.txt variable file to indicate pointers and variable names. Both should be placed in the Excavator2 working directory with this script being executed from there.
 
-source full_send_vars.sh
+source full_send_vars.txt
 cd $HOME$bamDir
 
-# Overwrite any existing config files
+## Overwrite existing config files and updating some vars
 rm -f ExperimentalFile*
+expName="${expName}_${window}_`date +"%Y-%m-%d_%H:%M"`"
 target="${expName}_target"
-runTp1=1 # step 1 TargetPerla.pl
-runDp2=1 # step 2 EXCAVATORDataPrepare.pl
-runDa3=1 # step 3 EXCAVATORDataAnalysis.pl
+# runTp1=1 # step 1 TargetPerla.pl
+# runDp2=1 # step 2 EXCAVATORDataPrepare.pl
+# runDa3=1 # step 3 EXCAVATORDataAnalysis.pl
 
-# Loop to iterate through each bam to create string for FilePrepare and FileAnalysis configs
+## Loop to iterate through each bam to create string for FilePrepare and FileAnalysis configs
 count=1
 for i in *.{bam,cram}; do
     echo "$PWD/$i" "data/prepare/$expName/${i%.*}" "${i%.*}" >> ExperimentalFilePrepare.$expName.txt
@@ -20,44 +21,52 @@ for i in *.{bam,cram}; do
     count=$[count+1]
 done
 
-# Function to remove leading path from sshfs local mount path and create control
+## Function to remove leading path from sshfs local mount path and create control
 sed -i -e 's/\/home\/chris/~/g' ./ExperimentalFilePrepare.$expName.txt
 sed -i '$s/T[[:digit:]]*/C1/g' ./ExperimentalFileAnalysis.$expName.txt
 
-# Relocate configs to Excavator working dir
+## Relocate configs to Excavator working dir
 mv ExperimentalFile* $HOME$excv2Dir
 mkdir -p $HOME$resDir/$expName
 mkdir -p $HOME$excv2Dir/config_files/$expName
 
-# Generate SourceTarget.txt and execute step 1 TargetPerla.pl
+## Generate SourceTarget.txt and execute step 1 TargetPerla.pl
 cd $HOME$excv2Dir
-if [[ $runTp1 = 1 ]]; then
-    echo "$HOME$bigWig" "$HOME$genRef" > SourceTarget.$expName.txt
-    perl TargetPerla.pl SourceTarget.$expName.txt $HOME$bedFile $target $window $refAssm;
-else
-    echo "Skipping step 1 TargetPerla.pl"
-fi
+echo "$HOME$bigWig" "$HOME$genRef" > SourceTarget.$expName.txt
+perl TargetPerla.pl SourceTarget.$expName.txt $HOME$bedFile $target $window $refAssm;
 
-# Executing step 2 EXCAVATORDataPrepare.pl
-if [[ $runDp2 = 1 ]]; then
-    perl EXCAVATORDataPrepare.pl -v ExperimentalFilePrepare.$expName.txt --processors $numProc --target $target --assembly $refAssm
-else
-    echo "Skipping step 2 EXCAVATORDataPrepare.pl"
-fi
+# if [[ $runTp1 = 1 ]]; then
+#     echo "$HOME$bigWig" "$HOME$genRef" > SourceTarget.$expName.txt
+#     perl TargetPerla.pl SourceTarget.$expName.txt $HOME$bedFile $target $window $refAssm;
+# else
+#     echo "Skipping step 1 TargetPerla.pl"
+# fi
 
-# Executing step 3 EXCAVATORDataAnalysis.pl
-if [[ $runDa3 = 1 ]]; then
-    perl EXCAVATORDataAnalysis.pl -v ExperimentalFileAnalysis.$expName.txt --processors $numProc --target $target --assembly $refAssm --output $HOME$resDir/$expName --mode $anMode
-else
-    echo "Skipping step 3 EXCAVATORDataAnalysis.pl"
-fi
 
-# Copy config files for record
+## Executing step 2 EXCAVATORDataPrepare.pl
+perl EXCAVATORDataPrepare.pl -v ExperimentalFilePrepare.$expName.txt --processors $numProc --target $target --assembly $refAssm
+
+# if [[ $runDp2 = 1 ]]; then
+#     perl EXCAVATORDataPrepare.pl -v ExperimentalFilePrepare.$expName.txt --processors $numProc --target $target --assembly $refAssm
+# else
+#     echo "Skipping step 2 EXCAVATORDataPrepare.pl"
+# fi
+
+## Executing step 3 EXCAVATORDataAnalysis.pl
+perl EXCAVATORDataAnalysis.pl -v ExperimentalFileAnalysis.$expName.txt --processors $numProc --target $target --assembly $refAssm --output $HOME$resDir/$expName --mode $anMode
+
+# if [[ $runDa3 = 1 ]]; then
+#     perl EXCAVATORDataAnalysis.pl -v ExperimentalFileAnalysis.$expName.txt --processors $numProc --target $target --assembly $refAssm --output $HOME$resDir/$expName --mode $anMode
+# else
+#     echo "Skipping step 3 EXCAVATORDataAnalysis.pl"
+# fi
+
+## Copy config files for record
 cp ParameterFile.txt ParameterFile.$expName.txt
-cp full_send_vars.sh full_send_vars.$expName.sh
-mv *.{$expname}.* $HOME$excv2Dir/config_files/$expName
+cp full_send_vars.txt full_send_vars.$expName.txt
+mv *.$expName.* $HOME$excv2Dir/config_files/$expName
 
-# Cleanup bad file naming convention
+## Cleanup bad file naming convention
 cd $HOME$resDir/$expName/Plots
 for i in */ ; do
     cd $i
